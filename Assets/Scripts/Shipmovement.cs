9ad852bd7b832f340e31d04d1b1236d10c05910c
soravitkung <soravit.saeh@bumail.net>
2026-03-09 22:12:13 +0700

    Scripts
    Movement ship

---
+++ Assets/Scripts/Shipmovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    [Header("Settings")]
+    public float speed = 20f; // ความเร็วของยาน
+
+    void Update()
+    {
+        // ทำให้ยานเคลื่อนที่ไปข้างหน้าตามทิศทางของตัวเอง (Local Forward)
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
